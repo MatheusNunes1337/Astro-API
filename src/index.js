aa77f38065a1e5dbb4dee0bce7eea7059f236eec
Matheus Nunes <matheusyagami147@gmail.com>
2020-11-24 15:01:03 -0300

    importação das rotas na index da aplicação atualizada

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import * as path from 'path'
-import routes from './routes'
+import routes from './routes/index.js'
 
 const app = express()
 dotenv.config()
