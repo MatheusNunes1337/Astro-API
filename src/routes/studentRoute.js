e991d805ca8ade3559f61c49d11e75c5aeda80a1
Matheus Nunes <matheusyagami147@gmail.com>
2020-11-26 14:24:05 -0300

    middleware de student inserido na rota de student find

--- src/routes/studentRoute.js
+++ src/routes/studentRoute.js
@@ -1,11 +1,12 @@
 import Router from 'express'
 import auth from '../middlewares/authentication'
 import studentController from '../controllers/studentController'
+import student from '../middlewares/student'
 
 const studentRoute = Router()
 
 studentRoute.get('/', studentController.index)
-studentRoute.get('/find', studentController.find)
+studentRoute.get('/find', student, studentController.find)
 studentRoute.post('/', studentController.create)
 studentRoute.delete('/:id', auth, studentController.delete) // auth
 studentRoute.put('/:id', auth, studentController.update) // auth
