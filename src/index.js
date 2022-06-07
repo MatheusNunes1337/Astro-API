29fa88e825d1542319603f3fde6591c607ece6b2
Matheus Nunes <matheus.silva@compasso.com.br>
2022-06-07 09:40:58 -0300

    feat: change * for astro domain in cors config

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 // cors
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', 'https://astrooo.herokuapp.com')
   app.use(cors())
   next()
 })
